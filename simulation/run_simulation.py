806eb57b3491fd5fb0a55502d605f60d86e9691d
Klaus Vogstad <klaus@windfarmdesigns.com>
2025-05-05 14:10:48 +0200

    knotet meg litt videre, og får kjørt wfm, men power blir nan

--- simulation/run_simulation.py
+++ simulation/run_simulation.py
@@ -8,12 +8,14 @@ from site_galvian.create_site import create_site_from_vortex
 from py_wake.wind_farm_models import PropagateDownwind
 from py_wake.deficit_models import NOJDeficit
 import pandas as pd
+import numpy as np
+import xarray as xr
 
 
 def main():
     site = create_site_from_vortex("Inputdata/vortex.serie.850535.6m 164m UTC-04.0 ERA5.txt")
     turbine = create_nordex_n164_turbine("Inputdata/Nordex N164.csv")
-    wfm = PropagateDownwind(site, turbine, NOJDeficit(site,turbine))
+    wfm = PropagateDownwind(site, turbine, wake_deficitModel=NOJDeficit())
 
     # Galvian layout
     turbine_coords = pd.read_csv("output/turbine_coordinates.csv")
@@ -25,8 +27,34 @@ def main():
     wd = site.ds['wind_direction'].values
     ws = site.ds['wind_speed'].values
 
-    sim_res = wfm(x, y, wd=wd, ws=ws, time=times)
+    sim_res = wfm(x, y, wd=wd, ws=ws, time=np.arange(len(times)))
     print("Power output per turbine:", sim_res.Power.values)
+    print("sim_res.Power.values.shape:", sim_res.Power.values.shape)
+    print("Number of time steps:", len(times))
+    print("Number of turbines:", len(x))
+
+    power = sim_res.Power.values
+    # If the first dimension is not time, transpose
+    if power.shape[0] != len(times):
+        print("Transposing power array to match (time, turbine) shape.")
+        power = power.T
+
+    # Create a new xarray Dataset with integer indices for time and turbine
+    ds = xr.Dataset(
+        {
+            "wind_speed": ("time", ws),
+            "wind_direction": ("time", wd),
+            "P": (("time", "turbine"), power),
+            "TI": ("time", 1/ws+0.04)
+        },
+        coords={
+            "time": np.arange(len(times)),
+            "turbine": np.arange(power.shape[1])
+        }
+    )
+
+    return sim_res
 
 if __name__ == "__main__":
-    main() 
+    main()
+    sim_res = main()
--- site_galvian/create_site.py
+++ site_galvian/create_site.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import xarray as xr
 from py_wake.site.xrsite import XRSite
+import numpy as np
 
 def create_site_from_vortex(file_path):
     """
@@ -25,20 +26,22 @@ def create_site_from_vortex(file_path):
         format="%Y%m%d%H%M"
     )
 
-    # Set datetime as index
-    df = df.set_index("datetime")
-
     # For time series, XRSite requires a 'P' variable (probability/time weight)
     P = [1.0] * len(df)
+
+    # Use integer indices for the 'time' coordinate
+    time_idx = np.arange(len(df))
 
     # Create xarray Dataset for XRSite
     ds = xr.Dataset(
         {
             "wind_speed": ("time", df["wind_speed"].values),
             "wind_direction": ("time", df["wind_direction"].values),
-            "P": ("time", P)
+            "P": ("time", P),
+            "TI": ("time", 1/df["wind_speed"].values+0.04),  # Using IEC 61400 simplified TI model TI = a/U+b
+            "datetime": ("time", df["datetime"].values)  # Store actual datetimes as a variable
         },
-        coords={"time": df.index}
+        coords={"time": time_idx}
     )
 
     site = XRSite(ds, interp_method='nearest')
